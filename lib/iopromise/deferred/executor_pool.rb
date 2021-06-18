687553edfe263ec8107bc184da211c17f24cc08b
Theo Julienne <theojulienne@github.com>
2021-06-18 08:32:38 +0000

    Fix DeferredExecutorPool to work with no other selectors.

--- lib/iopromise/deferred/executor_pool.rb
+++ lib/iopromise/deferred/executor_pool.rb
@@ -3,6 +3,15 @@
 module IOPromise
   module Deferred
     class DeferredExecutorPool < ::IOPromise::ExecutorPool::Batch
+      def initialize(*)
+        super
+
+        # register a dummy reader that never fires, to indicate to the event loop that
+        # there is a valid, active ExecutorPool.
+        @pipe_rd, @pipe_wr = IO.pipe
+        @iop_monitor = ::IOPromise::ExecutorContext.current.register_observer_io(self, @pipe_rd, :r)
+      end
+
       def execute_continue
         if @current_batch.empty?
           next_batch
